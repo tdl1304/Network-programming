183301d81ed69a1f9e8712321a5ba62e935a8a43
tdl1304 <tommy.1304@hotmail.com>
2021-02-28 14:07:47 +0100

    NettverkOblig 4 finished Implements worker threads and event loop with mutex locks and condition variables

---
+++ NettverkOblig4/src/Main.java
@@ -0,0 +1,43 @@
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Workers worker_threads = new Workers(4);
+        Workers event_loop = new Workers(1);
+        worker_threads.start();
+        event_loop.start();
+
+        worker_threads.post(()->{
+            System.out.println("Hello from task A: "+Thread.currentThread().getName());
+            return null;
+        });
+        worker_threads.post(()->{
+            System.out.println("Hello from task B: "+Thread.currentThread().getName());
+            return null;
+        });
+
+        event_loop.post(()->{
+            System.out.println("Hello from task C: "+Thread.currentThread().getName());
+            return null;
+        });
+
+        event_loop.post(()->{
+            System.out.println("Hello from task D: "+Thread.currentThread().getName());
+            return null;
+        });
+
+        worker_threads.post_timeout(()->{
+            System.out.println("Worker_thread timeout task just ran");
+            return null;
+        }, 6000);
+
+        event_loop.post_timeout(()->{
+            System.out.println("Eventloop timeout task just ran");
+            return null;
+        }, 2000);
+
+        worker_threads.join();
+        event_loop.join();
+
+        worker_threads.stop();
+        event_loop.stop();
+    }
+}
---
+++ NettverkOblig4/src/Workers.java
@@ -0,0 +1,89 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Workers {
+
+    private final Lock lock = new ReentrantLock();
+    private final Condition cv = lock.newCondition();
+    private final ExecutorService executor;
+    private final List<Callable<Object>> tasks = new ArrayList<>();
+    private boolean stopsignal = false;
+
+
+    public Workers(int poolsize) {
+        executor = Executors.newFixedThreadPool(poolsize);
+    }
+
+    public void post(Callable<Object> task) {
+        lock.lock();
+        tasks.add(task);
+        cv.signal();
+        lock.unlock();
+
+    }
+
+    public void start() {
+        Thread runner = new Thread(()->{
+            while (true) {
+                lock.lock();
+                try {
+                    cv.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                while (!tasks.isEmpty()) {
+                    executor.submit(tasks.remove(0));
+                }
+                if(stopsignal) {
+                    break;
+                }
+                lock.unlock();
+            }
+        });
+        runner.start();
+    }
+
+    /**
+     * Set timeout for task
+     *
+     * @param task  Callable
+     * @param delay millis to wait
+     */
+    public void post_timeout(Callable<Object> task, long delay) {
+        lock.lock();
+        tasks.add(() -> {
+            try {
+                Thread.sleep(delay);
+                task.call();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        });
+        cv.signal();
+        lock.unlock();
+
+    }
+
+    public void stop() {
+        lock.lock();
+        executor.shutdownNow();
+        stopsignal = true;
+        cv.signal();
+        System.out.println("shutdown finished");
+        lock.unlock();
+    }
+
+    public void join() {
+        executor.shutdown();
+        try {
+            executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        } catch (InterruptedException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
